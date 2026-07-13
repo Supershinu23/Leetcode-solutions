ee7aed824915c8da3fcb1647d2da097bde7d8268
Shrinivas Digambar Sontakke <shrinivassontakke13@gmail.com>
2026-07-14 00:54:52 +0530

    Create 215-KthLargestElementInAnArray

---
+++ 215-KthLargestElementInAnArray/215-KthLargestElementInAnArray.cpp
@@ -0,0 +1,8 @@
+// Last updated: 14/07/2026, 00:54:51
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()-k];
+    }
+};
