cca6d429546732abfe8d5e24b840ab5e15e52e41
Shrinivas Digambar Sontakke <shrinivassontakke13@gmail.com>
2026-07-14 00:54:39 +0530

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.cpp
@@ -0,0 +1,14 @@
+// Last updated: 14/07/2026, 00:54:39
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int i ;
+        sort(nums.begin(),nums.end()) ;
+        for(i=0;i<nums.size();i++){
+            if(i!=nums[i]){
+                break ;
+            }
+        }
+        return i ;
+    }
+};
